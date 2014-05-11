cb55cc3f816664881618242447b0d46d4a2dade3
Meena Shah <meena.shah@gmail.com>
2014-05-11 14:57:27 -0700

    Get eraser working properly
    eraser now works for paging next/prev also

--- pages/javascript_eraser.js
+++ pages/javascript_eraser.js
@@ -1,41 +1,34 @@
 var colors = []; 
-var pages = new Array("./pages/coverpage.png", "./pages/page1.png", "./pages/page2.png", "./pages/page3.png", "./pages/page4.png", "./pages/page5.png", "./pages/page6.png");
+var pages = new Array("url('./pages/coverpage.png')", "url('./pages/page1.png')", "url('./pages/page2.png')", "url('./pages/page3.png')", "url('./pages/page4.png')", "url('./pages/page5.png')", "url('./pages/page6.png')");
 var page_index = 0;
 var currentpage = pages[page_index]; 
 
 paintbrushes = new Array("./images/paintbrush_000000.png", "./images/paintbrush_7F7F7F.png", "./images/paintbrush_880015.png", "./images/paintbrush_DF013A.png", "./images/paintbrush_FF4000.png", "./images/paintbrush_FFFF00.png", "./images/paintbrush_04B431.png", "./images/paintbrush_0000FF.png", "./images/paintbrush_5F04B4.png", "./images/paintbrush_8904B1.png", "./images/paintbrush_FFFFFF.png", "./images/paintbrush_C3C3C3.png", "./images/paintbrush_B45F04.png", "./images/paintbrush_F781BE.png", "./images/paintbrush_FF8000.png", "./images/paintbrush_BFFF00.png", "./images/paintbrush_04B486.png", "./images/paintbrush_2ECCFA.png", "./images/paintbrush_DF01D7.png", "./images/paintbrush_FF00BF.png");  
-function color_change(index){
-	var colorEle = document.getElementById('color');
-	console.log(colorEle);
-	colorEle.selectedIndex  = index;
+function color_change(index, temp_ctx){
+	document.getElementById('color').selectedIndex = index; 
+	
 	// update the way the icons look
 	var color_brush = document.getElementById('colorbrush');
 	color_brush.src = paintbrushes[index];
 	var eraser = document.getElementById('eraser_img');
 	eraser.src = "./images/eraser.png";		
 	document.getElementById('brush').click();
-	
-	if (colors[this.value]) {
-		color = new colors[this.value]();
-	}
-	colorEle.change();
+	tmp_ctx.strokeStyle="#880015";
+
 }
 
 (function() {
 	
+	//var canvas = document.querySelector('#paint');
+	//var ctx = canvas.getContext('2d');
 	var canvas = document.querySelector('#paint');
-	var ctx = canvas.getContext('2d');
-	
-	var imageObj = new Image();
-	imageObj.onload = function() {
-    ctx.drawImage(imageObj, 0, 0);
-      };
-    imageObj.src = currentpage;
-	
+	var ctx = canvas.getContext('2d');  
 	var sketch = document.querySelector('#sketch');
 	var sketch_style = getComputedStyle(sketch);
-	canvas.width = parseInt(sketch_style.getPropertyValue('width'));
-	canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+	var tmp_canvas = document.createElement('canvas');
+	var tmp_ctx = tmp_canvas.getContext('2d');	
+	load_current_page();
+	
 	
 	// Determine Tool
 	var tool = 'pencil';
@@ -53,14 +46,7 @@ function color_change(index){
 	};
 	
 	
-	// Creating a tmp canvas
-	var tmp_canvas = document.createElement('canvas');
-	var tmp_ctx = tmp_canvas.getContext('2d');
-	tmp_canvas.id = 'tmp_canvas';
-	tmp_canvas.width = canvas.width;
-	tmp_canvas.height = canvas.height;
-	
-	sketch.appendChild(tmp_canvas);
+
 
 	var mouse = {x: 0, y: 0};
 	var last_mouse = {x: 0, y: 0};
@@ -80,7 +66,19 @@ function color_change(index){
       alert('Error: failed to get the dcolor element!');
       return;
     }
+	var color1 = document.getElementById('prevbutton');
+	color1.addEventListener('click', ev_color_change(1), false);	
+
+	
     color_select.addEventListener('change', ev_color_change, false);
+	
+	 // The event handler for any changes made to the color palette
+	  function ev_color_change (ev) {
+	  console.log("Color changed");
+		if (colors[this.value]) {
+		  color = new colors[this.value]();
+		}
+	  } 
 
 	document.querySelector('#size').onclick = function() {
 		console.log("event handler for size changer");
@@ -104,7 +102,7 @@ function color_change(index){
 	
 	
 	/* Drawing on Paint App */
-	tmp_ctx.lineWidth = 5;
+	tmp_ctx.lineWidth = 4;
 	tmp_ctx.lineJoin = 'round';
 	tmp_ctx.lineCap = 'round';
 	tmp_ctx.strokeStyle = 'black';
@@ -204,19 +202,20 @@ function color_change(index){
 	}
 
   function load_current_page(){
-  var property = "url('" + currentpage + ")";
-  sketch.style.background =  property;
-	var imageObj = new Image();
-	imageObj.onload = function() {
-		ctx.drawImage(imageObj, 0, 0);
-      };
-      imageObj.src = currentpage;  
+	canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+	canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+	// Creating a tmp canvas
+	tmp_canvas.id = 'tmp_canvas';
+	tmp_canvas.width = canvas.width;
+	tmp_canvas.height = canvas.height;	
+	sketch.appendChild(tmp_canvas);  
+  
+	var property = "url('" + currentpage + ")";
+	sketch.style.background =  currentpage;
   }
   
   // The event handler for the nextpage button
   function ev_nextpage (ev) {
-  console.log(pages.length);
-  console.log(page_index);
   console.log("event nextPage");
 	if (page_index < (pages.length - 1)){
 		page_index = page_index + 1;
